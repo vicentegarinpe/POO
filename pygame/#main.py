687862de53964cc412d0a42ec0c5e83f271cf669
vicentegarinpe <168034514+vicentegarinpe@users.noreply.github.com>
2024-09-11 11:45:55 -0300

    actualizacion de triangulo

--- pygame/#main.py
+++ pygame/#main.py
@@ -1,8 +1,37 @@
-#main.py
 import pygame
-
 from triangulo import Triangulo
+
 pygame.init()
 
-teclas=pygame.key_getpressed()
-pantalla=
+ventana = pygame.display.set_mode((800, 600)) 
+pygame.display.set_caption("Movimiento de Triangulo")
+
+background_color = (0, 0, 2)
+
+# Instancia de la clase Triangulo
+# Posición inicial del Triangulo en el plano
+triangulo = Triangulo(400, 100)  
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # Estado de las teclas
+    teclas = pygame.key.get_pressed()
+
+    # Mover el triángulo
+    triangulo.mover(teclas)
+
+    # Pintar el fondo
+    ventana.fill(background_color)
+
+    # Dibujar el triángulo
+    triangulo.dibujar(ventana)
+
+    # Actualizar la pantalla
+    pygame.display.update()
+
+# Cerrar Pygame
+pygame.quit()
--- pygame/triangulo.py
+++ pygame/triangulo.py
@@ -1,28 +1,24 @@
 import pygame
 
-class Triangulo():
+class Triangulo:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.velocidad = 0.05  # Velocidad de movimiento en pixeles
+        self.color = (219, 148, 255)  
 
-    def __init__(self,x,y):
-        self.x=x
-        self.y=y
-        self.color=(219,148,255)
-        self.velocidad=0,5
-    
+    def dibujar(self, ventana):
+        # Vertices del triángulo
+        puntos = [(self.x, self.y), (self.x - 50, self.y + 100), (self.x + 50, self.y + 100)]
+        pygame.draw.polygon(ventana, self.color, puntos)
 
-    def movimiento (self):
-        if teclas [pygame.K_LEFT]:
-            self.x -= self.velocidad 
-        if teclas [pygame.K_RIGHT]:
-            self.x+= self.velocidad
-        if teclas [pygame.K_UP]:
+    # Movimiento del triángulo según las teclas presionadas
+    def mover(self, teclas):
+        if teclas[pygame.K_LEFT]:
+            self.x -= self.velocidad
+        if teclas[pygame.K_RIGHT]:
+            self.x += self.velocidad
+        if teclas[pygame.K_UP]:
             self.y -= self.velocidad
-        if teclas [pygame.K_DOWN]:
+        if teclas[pygame.K_DOWN]:
             self.y += self.velocidad
-        
-
-
-    
-
-    def dibujar (self):
-        puntos=[(self.x,self.y),(self.x-50,self.y+100),(self.x+50,self.y+100)]
-        pygame.draw.polygon(ventana,self.color,puntos)
